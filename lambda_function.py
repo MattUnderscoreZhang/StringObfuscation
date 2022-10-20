7ffcb8c973554ccc4f484415a84da57ddc9daef3
Matt Zhang <set.stun@gmail.com>
2022-10-20 09:19:36 -0400

    Made lambda function CORS-compliant

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,15 @@
 import obfuscator
+from typing import Any
 
 
-def lambda_handler(event, context) -> dict[str, str]:
+def lambda_handler(event, context) -> dict[str, Any]:
     obfuscated_string = obfuscator.obfuscate_string(event["input"])
-    return {"output": obfuscated_string}
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'origin, content-type, accept',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': obfuscated_string
+    }
